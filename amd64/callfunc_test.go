3bdfef218d8cd58e09e0627013ca607a2b40478b
Nelson Elhage <nelhage@nelhage.com>
2014-04-28 19:43:06 -0700

    Add some more tests.

--- amd64/callfunc_test.go
+++ amd64/callfunc_test.go
@@ -2,6 +2,7 @@ package amd64
 
 import (
 	"github.com/nelhage/gojit"
+	"runtime"
 	"testing"
 )
 
@@ -19,6 +20,45 @@ func TestCallFunc(t *testing.T) {
 	if !called {
 		t.Error("CallFunc did not call the function")
 	}
+}
+
+func TestRecursion(t *testing.T) {
+	asm := newAsm(t)
+	defer gojit.Release(asm.Buf)
+
+	var jitf func(i int)
+	gof := func(i int) {
+		if i > 0 {
+			jitf(i - 1)
+		}
+	}
+
+	asm.Mov(Indirect{Rdi, 0, 64}, Rax)
+	asm.Push(Rax)
+	asm.CallFunc(gof)
+	asm.Pop(Rax)
+	asm.Ret()
+
+	gojit.BuildTo(asm.Buf, &jitf)
+
+	jitf(1024)
+}
+
+func TestGCInCallback(t *testing.T) {
+	asm := newAsm(t)
+	defer gojit.Release(asm.Buf)
+
+	gof := func(i int) {
+		runtime.GC()
+	}
+	var jitf func()
+
+	asm.CallFunc(gof)
+	asm.Ret()
+
+	gojit.BuildTo(asm.Buf, &jitf)
+
+	jitf()
 }
 
 func BenchmarkGoCall(b *testing.B) {
